a95df5f92750aecc4276eea25be6111848da493f
Niuuuuu <Wuted.cpen@gmail.com>
2014-11-20 00:08:13 -0800

    add menu screen to the game
    render a background for menu screen
    can switch between play, credit and help
    play button works now, other 2 buttons wont do anything right now

--- platformer.js
+++ platformer.js
@@ -81,11 +81,33 @@
   // UPDATE LOOP
   //-------------------------------------------------------------------------
 
+  var y_position = [680,950,1200];
+  var x_position = 1850;
+  var swtich_stat=0;
+  var game_stat =0; 
   function onkey(ev, key, down) {
     switch(key) {
       case KEY.LEFT:  player.left  = down; ev.preventDefault(); return false;
       case KEY.RIGHT: player.right = down; ev.preventDefault(); return false;
-      case KEY.SPACE: player.jump  = down; ev.preventDefault(); return false;
+      case KEY.SPACE: 
+            switch(game_stat){
+                case 0 : if(player.y == y_position[0]){ start_game();}
+                case 1 :player.jump  = down;
+
+            }ev.preventDefault(); return false;
+      case KEY.UP:     
+            switch(swtich_stat){
+              case 0 :player.x = x_position; player.y = y_position[0]; swtich_stat = 2; break;
+              case 1 :player.x = x_position; player.y = y_position[1]; swtich_stat = 0; break;
+              case 2 :player.x = x_position; player.y = y_position[2]; swtich_stat = 1; break; 
+             } ev.preventDefault(); return false;
+      case KEY.DOWN:     
+            switch(swtich_stat){
+              case 0 :player.x = x_position; player.y = y_position[0]; swtich_stat = 1; break;
+              case 1 :player.x = x_position; player.y = y_position[1]; swtich_stat = 2; break;
+              case 2 :player.x = x_position; player.y = y_position[2]; swtich_stat = 0; break; 
+             }  ev.preventDefault(); return false;
+
     }
   }
   
@@ -263,7 +285,7 @@
   }
 
   function renderPlayer(ctx, dt) {
-    ctx.fillStyle = COLOR.YELLOW;
+    ctx.fillStyle = COLOR.PINK;
     ctx.fillRect(player.x + (player.dx * dt), player.y + (player.dy * dt), TILE, TILE);
 
     var n, max;
@@ -352,7 +374,7 @@
 
   //-------------------------------------------------------------------------
   // THE GAME LOOP
-  //-------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
   
   var counter = 0, dt = 0, now,
       last = timestamp(),
@@ -373,24 +395,47 @@
     requestAnimationFrame(frame, canvas);
   }
   
+ 
+
+
   document.addEventListener('keydown', function(ev) { return onkey(ev, ev.keyCode, true);  }, false);
-  document.addEventListener('keyup',   function(ev) { return onkey(ev, ev.keyCode, false); }, false);
+  
 
   function start_game() {
+   swtich_stat= NaN;
+   game_stat =1;
+   GRAVITY  = 9.8 * 6;
+   document.addEventListener('keyup',   function(ev) { return onkey(ev, ev.keyCode, false); }, false);
+
   get("level_1.json", function(req) {
     setup(JSON.parse(req.responseText));
     frame();
   }); 
   } 
 
+  function start_menu() {
 
-if (confirm("wanna play???")){
+    GRAVITY  = 0;
+    game_stat =0;
+ 
+  get("menu.json", function(req) {
+    setup(JSON.parse(req.responseText));
+    frame();
+  }); 
+  } 
+
+
+
+if (confirm("click OK to directly go to the game , or click cancel to go to the menu screen")){
     window.alert("game starting!!");
     start_game();
 }
 else{
    
-    window.alert("why u no play");
+    window.alert("enterning menu screen");
+
+    start_menu();
+
 
 }
 
